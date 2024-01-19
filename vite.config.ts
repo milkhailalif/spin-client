f3aec72802ad03e09c687a7e6652263c969da443
milkhailalif <tom332435@gmail.com>
2024-01-19 02:29:54 -0500

    final-test

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import inject from '@rollup/plugin-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,11 +13,29 @@ export default defineConfig({
       assets: resolve(__dirname, "./src/assets"),
       components: resolve(__dirname, "./src/components"),
       constants: resolve(__dirname, "./src/constants"),
+      cryptoBrowserify_5: 'rollup-plugin-node-polyfills/polyfills/crypto-browserify/cryptoBrowserify',
     },
   },
-  plugins: [react()],
+  plugins: [,
+    nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      exclude: [
+        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }), react()],
   build: {
     chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })],
+    },
   },
   optimizeDeps: {
     esbuildOptions: {
